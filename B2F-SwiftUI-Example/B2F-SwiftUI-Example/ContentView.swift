a5e61f948a646fd30371e1c6ad4b4ad9c93b3a99
Rogelio <rogelio@bodygram.com>
2023-02-08 17:58:07 +0900

    deleting boilerplate

--- B2F-SwiftUI-Example/B2F-SwiftUI-Example/ContentView.swift
+++ B2F-SwiftUI-Example/B2F-SwiftUI-Example/ContentView.swift
@@ -10,13 +10,7 @@ import WebKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
     }
 }
 
