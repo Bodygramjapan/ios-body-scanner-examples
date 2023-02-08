1b31c41afab7020761bc700169f6ef2c1b3339ef
Rogelio <rogelio@bodygram.com>
2023-02-08 14:15:17 +0900

    adding empty SwiftUI project

---
+++ B2F-SwiftUI-Example/B2F-SwiftUI-Example/B2F_SwiftUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  B2F_SwiftUI_ExampleApp.swift
+//  B2F-SwiftUI-Example
+//
+//  Created by Rogelio Martinez Kobashi on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct B2F_SwiftUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
