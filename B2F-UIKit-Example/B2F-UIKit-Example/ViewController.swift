e61af148095e052c4288a5518d7fc8249bc109a6
Rogelio <rogelio@bodygram.com>
2023-02-09 16:39:54 +0900

    adding empty UIKit project

---
+++ B2F-UIKit-Example/B2F-UIKit-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  B2F-UIKit-Example
+//
+//  Created by Rogelio Martinez Kobashi on 2023/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
