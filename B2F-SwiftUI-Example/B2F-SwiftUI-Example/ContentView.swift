565a51c133a13fe24f5f0221ab02586a7176ff09
Rogelio <rogelio@bodygram.com>
2023-02-09 15:33:42 +0900

    adding BGWebView

--- B2F-SwiftUI-Example/B2F-SwiftUI-Example/ContentView.swift
+++ B2F-SwiftUI-Example/B2F-SwiftUI-Example/ContentView.swift
@@ -43,3 +43,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+private final class BGWebView: WKWebView, WKUIDelegate {
+  override init(frame: CGRect, configuration: WKWebViewConfiguration) {
+    super.init(frame: frame, configuration: configuration)
+    self.uiDelegate = self // need to set this delegate in order to access to the motion sensor
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
